96499c7192a052b9a19907b67b5e711d6f3d7c6d
Dan97O <dan97-oddo@live.it>
2023-06-05 17:54:23 +0200

    add edit app.blade

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -57,14 +57,16 @@
                                         <div class="modal-header">
                                             <h5 class="modal-title" id="modalTitleId">Delete: {{ $project->title }} </h5>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
+                                                aria-label="Close">
+                                            </button>
                                         </div>
                                         <div class="modal-body">
                                             This is a destructive action
                                         </div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-secondary text-light"
-                                                data-bs-dismiss="modal">Close</button>
+                                                data-bs-dismiss="modal">Close
+                                            </button>
                                             <form action="{{ route('admin.projects.destroy', $project->id) }}"
                                                 method="post">
                                                 @csrf
