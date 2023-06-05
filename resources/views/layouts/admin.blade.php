b1809c3588f0d76020943d15a0ac8a2877abde21
Dan97O <dan97-oddo@live.it>
2023-06-05 15:36:29 +0200

    add markup dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,3 +21,67 @@
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 </head>
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">BoolPress</a>
+            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse"
+                data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+            <ul class="navbar-nav px-3">
+                <li class="nav-item text-nowrap">
+                    <a class="nav-link" href="#">Sign out</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div class="container-fluid vh-100">
+            <div class="row h-100">
+                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                    <div class="position-sticky pt-3">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-dark' : '' }}"
+                                    aria-current="page" href="{{ route('admin.dashboard') }}">
+                                    <i class="fa-solid fa-gauge"></i>
+                                    {{ __('Dashboard') }}
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link {{ Route::currentRouteName() == 'admin.posts.index' ? 'bg-dark' : '' }}"
+                                    href="{{ route('admin.posts.index') }}">
+                                    <i class="fa-solid fa-thumbtack"></i>
+                                    {{ __('Posts') }}
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="#">
+                                    <i class="fa-solid fa-bookmark"></i>
+                                    Categories
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="#">
+                                    <i class="fa-solid fa-tags"></i>
+                                    Tags
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </nav>
+
+                <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+                    @yield('content')
+                </main>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
